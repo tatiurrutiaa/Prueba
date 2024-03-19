6ad4479c65f68b69a79465c1158b92e2d419213a
tatiurrutiaa <tati.u@live.com.ar>
2024-03-19 15:33:52 -0300

    cambio de prueba

--- Prueba/Program.cs
+++ Prueba/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace Prueba
+﻿using System.Security.Cryptography;
+
+namespace Prueba
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            Console.WriteLine(“Buen día”);
         }
     }
 }
